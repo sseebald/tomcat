2ee7880c690dfa73d5717ed61d37c5dea14bad87
Spencer Seebald <sseebald@gmail.com>
2015-01-12 16:14:39 -0800

    committing everything before moving to new dev env

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,29 +5,40 @@ class tomcat
   $port = undef,
 )
 {
-  
+  # Variable naming:
+  #   $service_name = Different service names based on version of tomcat
+  #   $tomcat_version = Thought here is to build the exe string for service creation...ie tomcat${tomcat_version}.exe
+
   if $version =~ /^5/ {
     $service_name = 'ACE'
+    $tomcat_version = '5'
   }
   elsif $version =~ /^6/ {
     $service_name = 'PFM'
+    $tomcat_version = '6'
   }
   elsif $version =~ /^7/ {
     $service_name = 'FCRA'
+    $tomcat_version = '7'
   }
   else {
     notify { "Unsupported version": }
   }
 
+  # Rudimentary java installation. Lot's that we can do here to build it out a bit. 
+  # First, let's just get it installed, all defaults for now.
+
   class { 'tomcat::java': }
 
   class { 'tomcat::package': 
-    version => $version,
+    version     => $version,
+    install_dir => $install_dir,
+    port        => $port,
   }
 
   #  class { 'tomcat::service':
   #    service_name => $service_name,
   #  }
 
-  notify { "current service_name would be $service_name": }
+  #  notify { "current service_name would be $service_name": }
 }
--- manifests/package.pp
+++ manifests/package.pp
@@ -1,31 +1,43 @@
 class tomcat::package 
 (
   $version,
-) 
+  $install_dir,
+  $port,
+)
+
 {
+  
+  # To-do tracking
+  #   - Need to fix package names as they are being 'created' each time since name doesn't match windows install name
+  #
+
   include profile::staging
-
-  #file { "C:/staging":
-  #  ensure => directory,
-  #  mode   => 0777,
-  #}
-
-  staging::file { "apache-tomcat-${version}.exe":
-    source  => "http://archive.apache.org/dist/tomcat/tomcat-6/v${version}/bin/apache-tomcat-${version}.exe",
-    #target  => "C:/staging",
-    #  require => File["C:/staging"],
+ 
+  staging::file { "apache-tomcat-6.0.43-windows-x64.zip":
+    source => "http://mirror.metrocast.net/apache/tomcat/tomcat-6/v6.0.43/bin/apache-tomcat-6.0.43-windows-x64.zip",
   }
 
-  file { "C:/staging/tomcat/apache-tomcat-${version}.exe": 
-    ensure  => present,
-    mode    => 0777,
-    require => Staging::File["apache-tomcat-${version}.exe"],
-  }
 
-  package { "tomcat":
-    ensure          => installed,
-    source          => "C:/staging/tomcat/apache-tomcat-${version}.exe",
-    require         => File["C:/staging/tomcat/apache-tomcat-${version}.exe"],
-    install_options => ['/S'],
-  }
+  
+  # What follows is a relic of original implementation. Does it work? Yeah, kinda. Tomcat exe doesn't allow you to 
+  # configure the application at all. The few parameters it does accept, /S and /D, are unreliable in my testing.
+  # Therefore, this is being sent to the comment graveyard for later reference, but the correct implementation is to
+  # be done through the .zip
+
+  #  staging::file { "apache-tomcat-${version}.exe":
+  #    source  => "http://archive.apache.org/dist/tomcat/tomcat-6/v${version}/bin/apache-tomcat-${version}.exe",
+  #  }
+  #
+  #  file { "C:/staging/tomcat/apache-tomcat-${version}.exe": 
+  #    ensure  => present,
+  #    mode    => 0777,
+  #    require => Staging::File["apache-tomcat-${version}.exe"],
+  #  }
+  #
+  #  package { "tomcat":
+  #    ensure          => installed,
+  #    source          => "C:/staging/tomcat/apache-tomcat-${version}.exe",
+  #    require         => File["C:/staging/tomcat/apache-tomcat-${version}.exe"],
+  #    install_options => ['/S', "/D=${install_dir}"],
+  #  }
 }
